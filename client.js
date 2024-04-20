bcf5dc02c21cd695811f208790e979662c6a7356
Wellurg <124252518+Wellurg1024@users.noreply.github.com>
2024-04-20 09:32:55 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,25 +1,46 @@
 const net = require('net');
+const readline = require('readline');
 
-// 连接到服务器
-function connectToServer() {
-    const socket = net.connect({ host: '192.168.0.103', port: 3000 }, () => {
-        console.log('已连接到服务器！');
-    });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    // 当接收到服务器发送的数据时，打印到终端
-    socket.on('data', (data) => {
-        let text = data.toString();
-        console.log(text);
-    });
+rl.question('ip > ', (hostIP) => {
+  console.log(`try connecting ${hostIP}!`);
+  connectToServer(hostIP);
+});
 
-    // 从终端读取用户输入，并发送给服务器
-    process.stdin.on('data', (data) => {
-        const input = data.toString().trim();
-        socket.write(data);
-    });
+function connectToServer(ip) {
+  const socket = net.connect({ host: ip, port: 3000 }, () => {
+    console.log('已连接到服务器！');
+  });
 
-    // 当与服务器断开连接时显示提示消息    socket.on('end', () => {
-        console.log('与服务器断开连接');
-    };
-    
-connectToServer()
+  // 当接收到服务器发送的数据时，打印到终端
+  socket.on('data', (data) => {
+    let text = data.toString();
+    console.log(text);
+  });
+
+  let userInput = ''; // 存储用户输入的变量
+
+  const stdinListener = (data) => {
+    userInput += data.toString();
+  };
+
+  process.stdin.on('data', stdinListener);
+
+  rl.on('line', () => {
+    socket.write(userInput.trim()); // 发送存储的用户输入
+    userInput = ''; // 重置用户输入
+    process.stdin.removeListener('data', stdinListener);
+    process.stdin.on('data', stdinListener); // 重新注册监听器，等待下一次用户输入
+  });
+
+  // 当与服务器断开连接时显示提示消息
+  socket.on('end', () => {
+    console.log('与服务器断开连接');
+    process.stdin.removeListener('data', stdinListener);
+    rl.close();
+  });
+}
