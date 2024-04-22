c71a352072a32aa9c5f2a8109050a09bd545628d
Wellurg <124252518+Wellurg1024@users.noreply.github.com>
2024-04-22 23:00:38 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,125 +1,150 @@
-const readline = require('readline');
+const fs = require('fs');
 const net = require('net');
 
-function scanLocalNetwork() {
-  const subnet = '192.168.0'; //change it to your local network
-  const port = 3000;
-  const timeout = 200;
 
-  const openHosts = [];
-  const openHostsIPs = [];
+const server = net.createServer();
 
-  const promises = [];
 
-  for (let i = 1; i <= 255; i++) {
-    const host = subnet + '.' + i;
-    const promise = new Promise((resolve) => {
-      const socket = new net.Socket();
-      socket.setTimeout(timeout);
+const clients = [];
 
-      socket.on('connect', () => {
-        openHosts.push(host);
-        openHostsIPs.push(host);
-        socket.destroy();
-        resolve();
-      });
+server.on('connection', (socket) => {
+    const clientIP = getClientIP(socket);
+ 
+        clients.push(socket);
 
-      socket.on('timeout', () => {
-        socket.destroy();
-        resolve();
-      });
 
-      socket.on('error', (error) => {
-        resolve();
-      });
+    const connectedMessage = `[${clientIP}] Connected!\n`;
+    sendToAllClientsAndConsole(connectedMessage);
 
-      socket.connect(port, host);
+
+    console.log(connectedMessage);
+
+
+    socket.on('data', (data) => {
+        const message = `[${clientIP}] ${data}`;
+        if (data.toString().trim() === '/stat') {
+            const statusMessage = getStatusMessage();
+            socket.write(statusMessage);
+        } else if (data.toString().startsWith('/ban ')) {
+            const ipToBan = '::ffff:' + data.toString().substring(5).trim();
+            banClientByIP(ipToBan);
+        } else if (data.toString().trim() === '/online') {
+            printOnlineIPv4Addresses();
+
+        } else {
+            sendToAllClientsAndConsole(message, socket);
+        }
     });
 
-    promises.push(promise);
-  }
-
-  return Promise.all(promises)
-    .then(() => {
-      return openHostsIPs;
+    socket.on('error', (error) => {
+        console.error(`Connect err with ${clientIP} :`, error.message);
     });
-}
 
-function connectToServer(ip) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 
-  const socket = net.connect({ host: ip, port: 3000 }, () => {
-    console.log('Connected to the server');
-  });
+    socket.on('end', () => {
+        const index = clients.indexOf(socket);
+        if (index !== -1) {
+            clients.splice(index, 1);
+            const disconnectedMessage = `[${clientIP}] have quit\n`;
+            sendToAllClientsAndConsole(disconnectedMessage);
+        }
+    });
+});
 
-  socket.on('data', (data) => {
-    let text = data.toString();
-    console.log(text);
-  });
 
-  let userInput = '';
-
-  const handleUserInput = (line) => {
-    const input = line.trim();
-  
-    if (line === '\u0008') {
-      if (userInput.length > 0) {
-        userInput = userInput.slice(0, -1);
-        readline.clearLine(process.stdout, 0);
-        readline.cursorTo(process.stdout, 0);
-        process.stdout.write(rl.prompt + userInput);
-      }
-    } else if (line) {
-      socket.write(input);
-      userInput = '';
+function getClientIP(socket) {
+    let ip = socket.remoteAddress;
+    if (typeof ip === 'string') {
+        if (ip.includes('::ffff:')) {
+            ip = ip.split(':').pop();
+        }
     }
-  };
-  rl.on('line', handleUserInput);
-
-
-  socket.on('end', () => {
-    console.log('End connection');
-    rl.close();
-  });
+    return ip;
 }
 
-scanLocalNetwork()
-  .then((openHostsIPs) => {
-    console.log('Open hosts:');
-    if (openHostsIPs.length > 0) {
-      openHostsIPs.forEach((host, index) => {
-        const coloredHost = `\x1b[32m${host.substring(host.lastIndexOf('.') + 1)}\x1b[0m`;
-        console.log(`${index + 1}. ${host.replace(/(\d+)$/, coloredHost)}`);
-      });
 
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
 
-      const getValidIndex = () => {
-        rl.question('plz input the index of ip u want to connect to：', (answer) => {
-          const selectedIndex = parseInt(answer);
-          if (!isNaN(selectedIndex) && selectedIndex >= 1 && selectedIndex <= openHostsIPs.length) {
-            const selectedIP = openHostsIPs[selectedIndex - 1];
-            console.log('You select：', selectedIP);
-            connectToServer(selectedIP);
-            rl.close();
-          } else {
-            console.log('plz select again');
-            getValidIndex();
-          }
-        });
-      };
+function sendToAllClientsAndConsole(message) {
+    console.log(message);
+    clients.forEach((client) => {
+        client.write(message);
+    });
+}
 
-      getValidIndex();
+
+function getStatusMessage() {
+    const memoryUsage = formatBytes(process.memoryUsage().rss);
+    const cpuTemperature = getCPUTemperature();
+
+    const onlineCount = clients.length;
+    const statusMessage = `Server Status:\nMemory Usage: ${memoryUsage}\nCPU Temperature: ${cpuTemperature}\nOnline Count: ${onlineCount}\n`;
+
+    return statusMessage;
+}
+
+function getOnlineIPv4Addresses() {
+    return clients.map((client) => {
+        let ip = client.remoteAddress;
+        // 检查 remoteAddress 是否存在且为字符串
+        if (typeof ip === 'string') {
+            // 判断是否为 IPv6 映射的 IPv4 地址，如果是，则提取出 IPv4 部分
+            if (ip.includes('::ffff:')) {
+                ip = ip.split(':').pop();
+            }
+            // 检查 ip 是否为有效的 IPv4 地址
+            if (net.isIPv4(ip)) {
+                return ip;
+            }
+        }
+        // 返回 null 表示无效地址
+        return null;
+    }).filter((ip) => ip !== null); // 过滤掉空地址
+}
+
+function printOnlineIPv4Addresses() {
+    const onlineIPv4Addresses = getOnlineIPv4Addresses();
+    if (onlineIPv4Addresses.length === 0) {
+        sendToAllClientsAndConsole('No online user\n');
     } else {
-      console.log('Find no host');
+        let addressList = 'Now online: \n';
+        onlineIPv4Addresses.forEach((address, index) => {
+            addressList += `${index + 1}. ${address}\n`;
+        });
+        sendToAllClientsAndConsole(addressList);
     }
-  })
-  .catch((error) => {
-    console.error('Err with net-scan', error);
-  });
+}
+
+
+
+function formatBytes(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Bytes';
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
+}
+
+
+function getCPUTemperature() {
+    try {
+        const temperature = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8');
+        return `${parseFloat(temperature) / 1000} °C`;
+    } catch (error) {
+        console.error('Error reading CPU temperature:', error);
+        return 'N/A';
+    }
+}
+
+
+function banClientByIP(ip) {
+    const bannedClients = clients.filter((client) => client.remoteAddress === ip);
+    bannedClients.forEach((client) => {
+        client.write('Server has just kicked u :( \n');
+        client.end();
+    });
+}
+
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
