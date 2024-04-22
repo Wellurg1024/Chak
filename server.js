c70a36396e5370c32edd283fe37b937d4e4b47c6
Wellurg <124252518+Wellurg1024@users.noreply.github.com>
2024-04-22 23:00:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 const net = require('net');
 
-// 创建一个服务器并监听指定端口
+
 const server = net.createServer();
 
-// 存储连接到服务器的客户端
+
 const clients = [];
 
 server.on('connection', (socket) => {
     const clientIP = getClientIP(socket);
-        // 将新连接的客户端添加到 clients 数组中
+ 
         clients.push(socket);
 
-    // 发送连接提示消息给客户端和控制台
-    const connectedMessage = `[${clientIP}] 已连接到服务器！\n`;
+
+    const connectedMessage = `[${clientIP}] Connected!\n`;
     sendToAllClientsAndConsole(connectedMessage);
 
-    // 发送连接提示消息给控制台
+
     console.log(connectedMessage);
 
-    // 当收到客户端发送的数据时，广播给所有连接的客户端
+
     socket.on('data', (data) => {
         const message = `[${clientIP}] ${data}`;
         if (data.toString().trim() === '/stat') {
@@ -37,26 +37,24 @@ server.on('connection', (socket) => {
     });
 
     socket.on('error', (error) => {
-        console.error(`与客户端 ${clientIP} 的连接发生错误:`, error.message);
+        console.error(`Connect err with ${clientIP} :`, error.message);
     });
 
-    // 当客户端断开连接时，从 clients 数组中移除并发送退出提示消息
+
     socket.on('end', () => {
         const index = clients.indexOf(socket);
         if (index !== -1) {
             clients.splice(index, 1);
-            const disconnectedMessage = `[${clientIP}] 已退出连接\n`;
+            const disconnectedMessage = `[${clientIP}] have quit\n`;
             sendToAllClientsAndConsole(disconnectedMessage);
         }
     });
 });
 
-// 获取客户端的 IP 地址
+
 function getClientIP(socket) {
     let ip = socket.remoteAddress;
-    // 检查 remoteAddress 是否存在且为字符串
     if (typeof ip === 'string') {
-        // 判断是否为 IPv6 映射的 IPv4 地址，如果是，则提取出 IPv4 部分
         if (ip.includes('::ffff:')) {
             ip = ip.split(':').pop();
         }
@@ -65,7 +63,7 @@ function getClientIP(socket) {
 }
 
 
-// 发送消息给所有连接的客户端和控制台
+
 function sendToAllClientsAndConsole(message) {
     console.log(message);
     clients.forEach((client) => {
@@ -73,7 +71,7 @@ function sendToAllClientsAndConsole(message) {
     });
 }
 
-// 获取服务器当前状态信息
+
 function getStatusMessage() {
     const memoryUsage = formatBytes(process.memoryUsage().rss);
     const cpuTemperature = getCPUTemperature();
@@ -103,13 +101,12 @@ function getOnlineIPv4Addresses() {
     }).filter((ip) => ip !== null); // 过滤掉空地址
 }
 
-// 在需要将消息发送到所有连接的客户端时，调用 sendToAllClients 函数
 function printOnlineIPv4Addresses() {
     const onlineIPv4Addresses = getOnlineIPv4Addresses();
     if (onlineIPv4Addresses.length === 0) {
-        sendToAllClientsAndConsole('当前没有在线客户端。\n');
+        sendToAllClientsAndConsole('No online user\n');
     } else {
-        let addressList = '当前在线客户端的IPv4地址列表：\n';
+        let addressList = 'Now online: \n';
         onlineIPv4Addresses.forEach((address, index) => {
             addressList += `${index + 1}. ${address}\n`;
         });
@@ -118,7 +115,7 @@ function printOnlineIPv4Addresses() {
 }
 
 
-// 格式化字节数，转换为可读的格式
+
 function formatBytes(bytes) {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return '0 Bytes';
@@ -126,7 +123,7 @@ function formatBytes(bytes) {
     return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
 }
 
-// 获取CPU温度
+
 function getCPUTemperature() {
     try {
         const temperature = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8');
@@ -137,17 +134,17 @@ function getCPUTemperature() {
     }
 }
 
-// 根据指定的IP断开与客户端的连接
+
 function banClientByIP(ip) {
     const bannedClients = clients.filter((client) => client.remoteAddress === ip);
     bannedClients.forEach((client) => {
-        client.write('服务器已将你踢出\n');
+        client.write('Server has just kicked u :( \n');
         client.end();
     });
 }
 
-// 启动服务器并监听指定端口
+
 const port = 3000;
 server.listen(port, () => {
-    console.log(`服务器已启动，监听端口 ${port}`);
+    console.log(`Server listening at ${port}`);
 });
