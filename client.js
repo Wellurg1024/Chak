9ed3d5b69f01008f8eae34178ba9c1e3c334aea0
Wellurg <124252518+Wellurg1024@users.noreply.github.com>
2024-04-22 23:02:33 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,150 +1,125 @@
-const fs = require('fs');
+const readline = require('readline');
 const net = require('net');
 
+function scanLocalNetwork() {
+  const subnet = '192.168.0'; // 更换为您的局域网子网
+  const port = 3000;
+  const timeout = 200;
 
-const server = net.createServer();
+  const openHosts = [];
+  const openHostsIPs = [];
 
+  const promises = [];
 
-const clients = [];
+  for (let i = 1; i <= 255; i++) {
+    const host = subnet + '.' + i;
+    const promise = new Promise((resolve) => {
+      const socket = new net.Socket();
+      socket.setTimeout(timeout);
 
-server.on('connection', (socket) => {
-    const clientIP = getClientIP(socket);
- 
-        clients.push(socket);
+      socket.on('connect', () => {
+        openHosts.push(host);
+        openHostsIPs.push(host);
+        socket.destroy();
+        resolve();
+      });
 
+      socket.on('timeout', () => {
+        socket.destroy();
+        resolve();
+      });
 
-    const connectedMessage = `[${clientIP}] Connected!\n`;
-    sendToAllClientsAndConsole(connectedMessage);
+      socket.on('error', (error) => {
+        resolve();
+      });
 
-
-    console.log(connectedMessage);
-
-
-    socket.on('data', (data) => {
-        const message = `[${clientIP}] ${data}`;
-        if (data.toString().trim() === '/stat') {
-            const statusMessage = getStatusMessage();
-            socket.write(statusMessage);
-        } else if (data.toString().startsWith('/ban ')) {
-            const ipToBan = '::ffff:' + data.toString().substring(5).trim();
-            banClientByIP(ipToBan);
-        } else if (data.toString().trim() === '/online') {
-            printOnlineIPv4Addresses();
-
-        } else {
-            sendToAllClientsAndConsole(message, socket);
-        }
+      socket.connect(port, host);
     });
 
-    socket.on('error', (error) => {
-        console.error(`Connect err with ${clientIP} :`, error.message);
+    promises.push(promise);
+  }
+
+  return Promise.all(promises)
+    .then(() => {
+      return openHostsIPs;
     });
+}
 
+function connectToServer(ip) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-    socket.on('end', () => {
-        const index = clients.indexOf(socket);
-        if (index !== -1) {
-            clients.splice(index, 1);
-            const disconnectedMessage = `[${clientIP}] have quit\n`;
-            sendToAllClientsAndConsole(disconnectedMessage);
-        }
-    });
-});
+  const socket = net.connect({ host: ip, port: 3000 }, () => {
+    console.log('Connection success!');
+  });
 
+  socket.on('data', (data) => {
+    let text = data.toString();
+    console.log(text);
+  });
 
-function getClientIP(socket) {
-    let ip = socket.remoteAddress;
-    if (typeof ip === 'string') {
-        if (ip.includes('::ffff:')) {
-            ip = ip.split(':').pop();
-        }
+  let userInput = ''; // 存储用户输入的变量
+
+  const handleUserInput = (line) => {
+    const input = line.trim();
+  
+    if (line === '\u0008') { // 检测到 Backspace 输入，删除输入信息的最后一个字符
+      if (userInput.length > 0) {
+        userInput = userInput.slice(0, -1);
+        readline.clearLine(process.stdout, 0); // 清除终端上当前行的内容
+        readline.cursorTo(process.stdout, 0); // 将光标移动到行首
+        process.stdout.write(rl.prompt + userInput); // 重新显示输入信息
+      }
+    } else if (line) { // 发送用户输入的信息
+      socket.write(input);
+      userInput = ''; // 清空用户输入
     }
-    return ip;
+  };
+  rl.on('line', handleUserInput);
+
+  // 当与服务器断开连接时显示提示消息
+  socket.on('end', () => {
+    console.log('lost connection');
+    rl.close();
+  });
 }
 
+scanLocalNetwork()
+  .then((openHostsIPs) => {
+    console.log('Open hosts:');
+    if (openHostsIPs.length > 0) {
+      openHostsIPs.forEach((host, index) => {
+        const coloredHost = `\x1b[32m${host.substring(host.lastIndexOf('.') + 1)}\x1b[0m`;
+        console.log(`${index + 1}. ${host.replace(/(\d+)$/, coloredHost)}`);
+      });
 
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
 
-function sendToAllClientsAndConsole(message) {
-    console.log(message);
-    clients.forEach((client) => {
-        client.write(message);
-    });
-}
-
-
-function getStatusMessage() {
-    const memoryUsage = formatBytes(process.memoryUsage().rss);
-    const cpuTemperature = getCPUTemperature();
-
-    const onlineCount = clients.length;
-    const statusMessage = `Server Status:\nMemory Usage: ${memoryUsage}\nCPU Temperature: ${cpuTemperature}\nOnline Count: ${onlineCount}\n`;
-
-    return statusMessage;
-}
-
-function getOnlineIPv4Addresses() {
-    return clients.map((client) => {
-        let ip = client.remoteAddress;
-        // 检查 remoteAddress 是否存在且为字符串
-        if (typeof ip === 'string') {
-            // 判断是否为 IPv6 映射的 IPv4 地址，如果是，则提取出 IPv4 部分
-            if (ip.includes('::ffff:')) {
-                ip = ip.split(':').pop();
-            }
-            // 检查 ip 是否为有效的 IPv4 地址
-            if (net.isIPv4(ip)) {
-                return ip;
-            }
-        }
-        // 返回 null 表示无效地址
-        return null;
-    }).filter((ip) => ip !== null); // 过滤掉空地址
-}
-
-function printOnlineIPv4Addresses() {
-    const onlineIPv4Addresses = getOnlineIPv4Addresses();
-    if (onlineIPv4Addresses.length === 0) {
-        sendToAllClientsAndConsole('No online user\n');
-    } else {
-        let addressList = 'Now online: \n';
-        onlineIPv4Addresses.forEach((address, index) => {
-            addressList += `${index + 1}. ${address}\n`;
+      const getValidIndex = () => {
+        rl.question('plz input the index', (answer) => {
+          const selectedIndex = parseInt(answer);
+          if (!isNaN(selectedIndex) && selectedIndex >= 1 && selectedIndex <= openHostsIPs.length) {
+            const selectedIP = openHostsIPs[selectedIndex - 1];
+            console.log('You select: ', selectedIP);
+            connectToServer(selectedIP);
+            rl.close();
+          } else {
+            console.log('无效的索引，请重新输入有效的数字索引。');
+            getValidIndex(); // 递归调用重新获取有效的索引
+          }
         });
-        sendToAllClientsAndConsole(addressList);
+      };
+
+      getValidIndex();
+    } else {
+      console.log('No avaliable');
     }
-}
-
-
-
-function formatBytes(bytes) {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return '0 Bytes';
-    const i = Math.floor(Math.log(bytes) / Math.log(1024));
-    return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
-}
-
-
-function getCPUTemperature() {
-    try {
-        const temperature = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp', 'utf-8');
-        return `${parseFloat(temperature) / 1000} °C`;
-    } catch (error) {
-        console.error('Error reading CPU temperature:', error);
-        return 'N/A';
-    }
-}
-
-
-function banClientByIP(ip) {
-    const bannedClients = clients.filter((client) => client.remoteAddress === ip);
-    bannedClients.forEach((client) => {
-        client.write('Server has just kicked u :( \n');
-        client.end();
-    });
-}
-
-
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server listening at ${port}`);
-});
+  })
+  .catch((error) => {
+    console.error('err with net-scanning', error);
+  });
